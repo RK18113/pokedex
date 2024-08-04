e2246aacfd20a8434066f6bc4ece9203dc3ecde7
RK18113 <ramakrishnanilanghovan@gmail.com>
2024-08-04 23:47:09 +0530

    added fetch api function, updated pokemon image, updated ui

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,27 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Pokedex() {
+  const [pokemonName, setPokemonName] = useState('');
+  const [image, setImage] = useState();
+  const [details, setDetails] = useState(null);
 
-  
+  async function getData(){
+    try {
+      const response = pokemonName ? await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`) : await fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`);
+      const pkmnDetails = await response.json();
+      console.log(pkmnDetails);
+      setDetails(pkmnDetails);
+    } catch (error) {
+      console.error("error fetching");
+      throw error;
+    }
+  }
 
   return (
     <div className='flex justify-center items-center h-screen w-screen bg-black'>
         <div className='flex justify-center flex-nowrap h-[80vh] w-[40vw] bg-red-500 rounded-3xl'>
           <div className='w-[60%]'>
-            <div className='h-[40%] mt-5 mr-5 ml-5 mb-0 bg-white rounded-xl'>
+            <div className='flex justify-center h-[40%] mt-5 mr-10 ml-10 mb-0 bg-white rounded-xl'>
               {/* pokemon image */}
-              <img src="" alt="" /> 
+              <img src={details ? details.sprites.front_default : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"} alt="" /> 
             </div>
 
             <div className='flex flex-wrap justify-center'>
-              <label htmlFor="pokemonName" className='w-full text-center h-[30px] m-0 mt-5'>Enter Pokemon Name</label>
-              <input type="text" name='pokemonName' className=' m-0 rounded-xl' />
-              <button onClick className='ml-3 bg-yellow-400 rounded-xl p-2'>Search</button>            
+              <label htmlFor="pokemonName" className='w-full text-center h-[30px] m-0 mt-5 mb-2 text-xl font-extrabold font-serif'>Enter Pokemon Name</label>
+              <input type="text" name='pokemonName' className='p-1 m-0 rounded-xl text-center font-semibold' onChange={(event) => setPokemonName(event.target.value)} />
+              <button onClick = {getData} className='p-2 ml-3 bg-yellow-400 rounded-xl font-bold font-serif border-[2px] border-black hover:bg-yellow-500'>Search</button>            
             </div>
 
             <div className='flex justify-around mt-10 ml-5 mr-5'>
-              <button className='h-[5vw] w-[5vw] rounded-3xl bg-yellow-300'>&lt;</button>
-              <button className='h-[5vw] w-[5vw] rounded-3xl bg-yellow-300'>&gt;</button>
+              <button className='h-[5vw] w-[5vw] rounded-3xl bg-yellow-300 text-3xl font-extrabold font-mono border-[2px] border-black hover:bg-yellow-400'>&lt;</button>
+              <button className='h-[5vw] w-[5vw] rounded-3xl bg-yellow-300 text-3xl font-extrabold font-mono border-[2px] border-black hover:bg-yellow-400'>&gt;</button>
             </div>
           </div>
 
